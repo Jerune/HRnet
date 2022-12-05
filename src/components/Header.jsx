a8dff5b207c93defea6e37dd35df791e39e5a522
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-12-05 10:39:09 +0100

    feat: Setup Header
    Created a header with Company name, links and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <nav className="nav">
+      <h1>HRnet</h1>
+      <div className="nav-menu">
+        <NavLink className="nav-item" to="/">
+          Create Employee
+        </NavLink>
+        <NavLink className="nav-item" to="/employee-list">
+          Employee List
+        </NavLink>
+      </div>
+    </nav>
+  )
 }
 
 export default Header
