fbd68fdb593a27461085beb739accd3f04f84333
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-12-13 10:31:49 +0100

    feat: Adding Mocked Employees for testing purposes
    Now we can use mocked employees file to test data tables functionality

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,18 +1,21 @@
 // @ts-nocheck
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import DataTable from 'react-data-table-component'
+import { mockedEmployees } from '../data/mockedEmployees'
 import { Context } from '../store'
 import getTableColumns from '../utils/getTableColumns'
 
-const ExpandedComponent = ({ data }) => (
-  <pre>{JSON.stringify(data, null, 2)}</pre>
-)
 const columns = getTableColumns()
 
 const EmployeeList = () => {
   const { employeeList } = useContext(Context)
+  const [filteredEmployees, setFilteredEmployees] = useState(mockedEmployees)
 
-  return <DataTable columns={columns} data={employeeList} pagination />
+  return (
+    <>
+      <DataTable columns={columns} data={filteredEmployees} pagination />
+    </>
+  )
 }
 
 export default EmployeeList
